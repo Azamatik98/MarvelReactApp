e3f124ab73006d9bae17268e6254180ccde28d44
azamat <iskanderovak98@gmail.com>
2022-10-20 10:25:00 +0300

    add setContent

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,34 +1,55 @@
-class MarvelService {
-  _apiBase = "https://gateway.marvel.com:443/v1/public/";
-  _apiKey = "apikey=4aef6ec5e4b94d0a9bd7fd1ab3ac1657";
-  _baseOffset = 210;
+import { useHttp } from "../hooks/http.hook";
 
-  getResource = async (url) => {
-    let result = await fetch(url);
+const useMarvelService = () => {
+  const { loading, request, error, clearError } = useHttp();
 
-    if (!result.ok) {
-      throw new Error(`Cloud not fetch ${url}, status: ${result.status}`);
-    }
+  const _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  const _apiKey = "apikey=4aef6ec5e4b94d0a9bd7fd1ab3ac1657";
+  const _baseOffset = 210;
 
-    return await result.json();
+  const getAllCharacters = async (offset = _baseOffset) => {
+    const result = await request(
+      `${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
+    );
+    return result.data.results.map(_transformCharacter);
   };
 
-  getAllCharacters = async (offset = this._baseOffset) => {
-    const result = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
+  // Вариант модификации готового метода для поиска по имени.
+  // Вызывать его можно вот так: getAllCharacters(null, name)
+
+  // const getAllCharacters = async (offset = _baseOffset, name = '') => {
+  //     const res = await request(`${_apiBase}characters?limit=9&offset=${offset}${name ? `&name=${name}` : '' }&${_apiKey}`);
+  //     return res.data.results.map(_transformCharacter);
+  // }
+
+  // Или можно создать отдельный метод для поиска по имени
+
+  const getCharacterByName = async (name) => {
+    const result = await request(
+      `${_apiBase}characters?name=${name}&${_apiKey}`
     );
-    return result.data.results.map(this._transformCharacter);
+    return result.data.results.map(_transformCharacter);
   };
 
-  getCharacter = async (id) => {
-    const result = await this.getResource(
-      `${this._apiBase}characters/${id}?&${this._apiKey}`
+  const getCharacter = async (id) => {
+    const result = await request(`${_apiBase}characters/${id}?&${_apiKey}`);
+    return _transformCharacter(result.data.results[0]);
+  };
+
+  const getAllComics = async (offset = 0) => {
+    const result = await request(
+      `${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
     );
-    return this._transformCharacter(result.data.results[0]);
+    return result.data.results.map(_transformComics);
+  };
+
+  const getComic = async (id) => {
+    const result = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+    return _transformComics(result.data.results[0]);
   };
 
   // получение данных из API
-  _transformCharacter = (char) => {
+  const _transformCharacter = (char) => {
     return {
       id: char.id,
       name: char.name,
@@ -41,6 +62,31 @@ class MarvelService {
       comics: char.comics.items,
     };
   };
-}
 
-export default MarvelService;
+  const _transformComics = (comics) => {
+    return {
+      id: comics.id,
+      title: comics.title,
+      description: comics.description || "There is no description",
+      pageCount: comics.pageCount
+        ? `${comics.pageCount} p.`
+        : "No information about the number of pages",
+      thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+      language: comics.textObjects.language || "en-us",
+      price: comics.prices.price ? `${comics.prices.price}$` : "not available",
+    };
+  };
+
+  return {
+    loading,
+    error,
+    getAllCharacters,
+    getCharacterByName,
+    getCharacter,
+    clearError,
+    getComic,
+    getAllComics,
+  };
+};
+
+export default useMarvelService;
